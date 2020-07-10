7cd9d1156620b416984e00a66cdc44ec971c68ff
Arne Varholm <arne.varholm@km.kongsberg.com>
2020-07-10 14:58:35 +0200

    Set Qt5 path for Windows, do not set C/CXX Flags for Windows.

--- cmake/AddCompilerFlags.cmake
+++ cmake/AddCompilerFlags.cmake
@@ -3,7 +3,7 @@
 #                                                                                                                      #
 #                                                                                                                      #
 #----------------------------------------------------------------------------------------------------------------------#
-if(${CMAKE_SYSTEM_NAME} NOT MATCHES "Linux")
+if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -Wall -Wextra -Wpedantic")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic")
 endif()
